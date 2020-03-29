b4476738262243a41ff1364af1ad6ddddb9ec8fb
nicolasariza <nicolas_ariza@outlook.com>
2020-03-29 13:23:40 -0500

    Updates

---
+++ precache-manifest.61119a612713fc4622633b8894810087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96de25129df68d7b40ca96f73a2aba0",
+    "url": "/gastos/index.html"
+  },
+  {
+    "revision": "ae7ef656cda5a302a570",
+    "url": "/gastos/static/css/main.db6c011e.chunk.css"
+  },
+  {
+    "revision": "47738335807804be2c17",
+    "url": "/gastos/static/js/2.395ab5d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gastos/static/js/2.395ab5d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7ef656cda5a302a570",
+    "url": "/gastos/static/js/main.388da64c.chunk.js"
+  },
+  {
+    "revision": "f96e879de4f6d1b79ab2",
+    "url": "/gastos/static/js/runtime-main.172f1644.js"
+  },
+  {
+    "revision": "7bc3642c31f48df1dff223e3f8929fd1",
+    "url": "/gastos/static/media/fondo.7bc3642c.jpeg"
+  }
+]);
